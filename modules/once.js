3da1652d6cf4bbdc305f4088df1e24d6a2a27fe7
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-19 10:02:30 +0800

    @wzm

--- modules/once.js
+++ modules/once.js
@@ -5,3 +5,4 @@ import before from './before.js';
 // Returns a function that will be executed at most one time, no matter how
 // often you call it. Useful for lazy initialization.
 export default partial(before, 2);
+ 
