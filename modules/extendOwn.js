4657c8a307e491c630f7550638379878396e20b5
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-20 17:53:25 +0800

    @wzm

--- modules/extendOwn.js
+++ modules/extendOwn.js
@@ -5,4 +5,4 @@ import keys from './keys.js';
 // Assigns a given object with all the own properties in the passed-in
 // object(s).
 // (https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Object/assign)
-export default createAssigner(keys);
+export default createAssigner(keys); 
