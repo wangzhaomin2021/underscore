737803f04cb12fc4747bfea1c447c1fa18912770
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-21 10:37:18 +0800

    @wzm

--- modules/find.js
+++ modules/find.js
@@ -9,3 +9,14 @@ export default function find(obj, predicate, context) {
   var key = keyFinder(obj, predicate, context);
   if (key !== void 0 && key !== -1) return obj[key];
 }
+
+/*
+function find(arrOrObject, cb, thisArg) {
+	const keys = Array.isArray(arrOrObject) ? null : Object.keys(arrOrObject)
+	const len = keys ? keys.length : arrOrObject.length
+	for(let i = 0; i < len; i++) {
+		const key = keys ? keys[i] : i
+		if (cb.call(thisArg || null, arrOrObject[key], key, arrOrObject)) return arrOrObject[key]
+	}
+}
+*/
