dd8e91fdf359d4c08f29568de7d65b8a5a61095e
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2022-12-23 10:24:43 +0800

    @wzm

--- modules/memoize.js
+++ modules/memoize.js
@@ -1,3 +1,4 @@
+// @wzm 2022-12-23
 import has from './_has.js';
 
 // Memoize an expensive function by storing its results.
@@ -11,3 +12,21 @@ export default function memoize(func, hasher) {
   memoize.cache = {};
   return memoize;
 }
+
+/*
+
+function memorize(fn, hasher) {
+	const memFn = function(...args) {
+		const cache = memFn.cache
+		const mem_key = hasher ? hasher.apply(this, args) : args[0]
+		// if (mem_key in cache) return cache[mem_key]
+		// else return cache[mem_key] = fn.apply(this, args)
+		
+		return mem_key in cache ? cache[mem_key] : cache[mem_key] = fn.apply(this, args)
+	}
+	
+	memFn.cache = {}
+	return memFn
+}
+
+*/
