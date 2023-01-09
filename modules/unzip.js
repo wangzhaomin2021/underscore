025cccb2d87e44ece48ecf16349cf74c217ac4b0
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2023-01-09 10:06:07 +0800

    @wzm

--- modules/unzip.js
+++ modules/unzip.js
@@ -1,3 +1,4 @@
+// @wzm 2023-1-9
 import max from './max.js';
 import getLength from './_getLength.js';
 import pluck from './pluck.js';
@@ -13,3 +14,16 @@ export default function unzip(array) {
   }
   return result;
 }
+
+/*
+function unzip(...arrays) {
+	const arrs = []
+	arrays.forEach((array, index) => {
+		array.forEach((item, idx) => {
+			!arrs[idx] && (arrs[idx] = [])
+			arrs[idx][index] = item
+		})
+	})
+	return arrs
+}
+*/
