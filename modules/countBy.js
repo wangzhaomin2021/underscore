5d9079f3fc26d4ee3dec718445f34578cf9cb883
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2023-01-12 11:32:03 +0800

    @wzm

--- modules/countBy.js
+++ modules/countBy.js
@@ -8,3 +8,21 @@ import has from './_has.js';
 export default group(function(result, value, key) {
   if (has(result, key)) result[key]++; else result[key] = 1;
 });
+
+/*
+function countBy(list, iteratee, context) {
+	const ret = {}
+	if (context && typeof iteratee === 'function') iteratee = iteratee.bind(content)
+	const type = typeof iteratee === 'string' ? 'string' : typeof iteratee === 'function' ? 'function' : 'error'
+	if (type === 'error') return list
+	const iter = type === 'string' ? (item) => item[iteratee] : iteratee  // 统一迭代器
+	
+	list.forEach((item, index, ctx) => {
+		const r = iter(item, index, ctx)
+		if (!ret[r]) ret[r] = 1
+		else ret[r] += 1
+	})
+	
+	return ret
+}
+*/
