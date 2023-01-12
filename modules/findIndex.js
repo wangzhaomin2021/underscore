30dc9e362debabd9dfe34ad2eff2bdd15382a028
wangzhaomin2021 <89674829+wangzhaomin2021@users.noreply.github.com>
2023-01-12 11:24:09 +0800

    @wzm

--- modules/findIndex.js
+++ modules/findIndex.js
@@ -2,4 +2,4 @@
 import createPredicateIndexFinder from './_createPredicateIndexFinder.js';
 
 // Returns the first index on an array-like that passes a truth test.
-export default createPredicateIndexFinder(1);
+export default createPredicateIndexFinder(1); 
